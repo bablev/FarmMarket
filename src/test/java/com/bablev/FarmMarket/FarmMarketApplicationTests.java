8a6372ac815cf2bafc2faed1ee669b257c2b2479
bablev <zhenyafarter@gmail.com>
2025-02-09 21:45:00 +0300

    init commit

---
+++ src/test/java/com/bablev/FarmMarket/FarmMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bablev.FarmMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
